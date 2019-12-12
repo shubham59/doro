7b0525b55aba14fa144728af63bdbfba1ce49e85
Shubham <shub1401@gitlab.quadeyesecurities.com>
2019-12-13 03:03:11 +0530

    Initial Commit

---
+++ doro/adopter/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class UserProfile(User):
+    user_name = models.CharField(null=True, blank=True, max_length=100)
+    mobile = models.CharField(null=False, blank=False, max_length=12)
+
+    def __str__(self):
+        return self.user_name
